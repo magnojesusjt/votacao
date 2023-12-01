e453e95b2b593dcb9fd9d12c24491b39a70f501e
Magnovaldo Teixeira <magnovaldo.teixeira@ssp.ba.gov.br>
2023-12-01 10:19:34 -0300

    dados url

--- server/main.js
+++ server/main.js
@@ -1,10 +1,9 @@
-import { Meteor } from 'meteor/meteor'
-import { LinksCollection } from '/imports/api/links'
-import './methods/getCPf'
+import { Meteor } from "meteor/meteor";
+import { LinksCollection } from "/imports/api/links";
+import "./methods/getCPf";
 
-import '../imports/api/votacaoTotal'
+import "../imports/api/votacaoTotal";
 
 Meteor.startup(async () => {
-
-
-})
+  process.env.ROOT_URL = "http://10.91.16.136:3000/";
+});
